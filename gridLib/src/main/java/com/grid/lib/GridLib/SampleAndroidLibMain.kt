6ee77fb603669bdda28eb409775f9d110bcce74c
MaulikMikka <61727670+MaulikMikka@users.noreply.github.com>
2021-09-07 19:18:32 +0530

    Init

---
+++ gridLib/src/main/java/com/grid/lib/GridLib/SampleAndroidLibMain.kt
@@ -0,0 +1,3 @@
+package com.grid.lib.GridLib
+
+class SampleAndroidLibMain(val welcomeString: String = "Welcome to the sample implementation of an Android Library project published on GitHub Packages Registry")
